20960ab275323eb17cba4acb731e8eeda82d41d4
Theo <jhn_hawley@hotmail.com>
2025-06-05 15:12:27 -0800

    added node_delimiter and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,9 +1,11 @@
 import unittest
 
 from textnode import TextNode, TextType
+from node_delimiter import split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
+#these test textnode.py
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
@@ -28,6 +30,59 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, "https://www.badtest.com")
         node2 = TextNode("This is a text node", TextType.BOLD, "https://www.test.com")
         self.assertNotEqual(node, node2)
+
+#these test node_delimiter.py
+    def test_delim_bold(self):
+        node = TextNode("this is a **BOLD** word.", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual([
+            TextNode("this is a ", TextType.TEXT),
+            TextNode("BOLD", TextType.BOLD),
+            TextNode(" word.", TextType.TEXT)],
+            new_nodes
+        )
+
+    def test_delim_double_bold(self):
+        node = TextNode("this **is** a **BOLD** word.", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual([
+            TextNode("this ", TextType.TEXT),
+            TextNode("is", TextType.BOLD),
+            TextNode(" a ", TextType.TEXT),
+            TextNode("BOLD", TextType.BOLD),
+            TextNode(" word.", TextType.TEXT)],
+            new_nodes
+        )
+
+    def test_delim_italic_and_bold(self):
+        node = TextNode("this is a **BOLD** and a _ITALIC_ word.", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+        expected = [
+            TextNode("this is a ", TextType.TEXT),
+            TextNode("BOLD", TextType.BOLD),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("ITALIC", TextType.ITALIC),
+            TextNode(" word.", TextType.TEXT)]
+        self.assertEqual(new_nodes, expected)
+    
+    def test_delim_uneven_delimiter(self):
+        node = TextNode("this **is** a **BOLD* word.", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+            
+    def test_delim_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
