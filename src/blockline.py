63c5df605b46d26b4d70766944ee043d09bd491e
Theo <jhn_hawley@hotmail.com>
2025-06-12 18:19:12 -0800

    started the markdown_to_html_nodes and helper functions/tests

--- src/blockline.py
+++ src/blockline.py
@@ -1,4 +1,9 @@
 from enum import Enum
+from textnode import TextNode, TextType
+from node_delimiter import text_to_textnodes, split_nodes_delimiter, split_nodes_image, split_nodes_link
+from text_to_html import text_node_to_html_node
+from htmlnode import HTMLNode, LeafNode, ParentNode
+
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -43,3 +48,94 @@ def block_to_block_type(md_block):
         return BlockType.ORDERED_LIST
     else:
         return BlockType.PARAGRAPH
+    
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    html_nodes = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == BlockType.PARAGRAPH:
+            html_nodes.append(paragraph_to_html_node(block))
+        if block_type == BlockType.HEADING:
+            html_nodes.append(heading_to_html_node(block))
+        if block_type == BlockType.QUOTE:
+            html_nodes.append(quote_to_html_node(block))
+        if block_type == BlockType.CODE:
+            html_nodes.append(code_to_html_node(block))
+    return ParentNode("div", html_nodes)
+
+
+def paragraph_to_html_node(block_text):
+    text_nodes = text_to_textnodes(block_text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return ParentNode("p", children)
+
+def heading_to_html_node(block_text):
+    children = []
+    tag = None
+    if block_text.startswith("# "):
+        tag = "h1"
+        heading_text = block_text[2:]
+        text_nodes = text_to_textnodes(heading_text)
+        for node in text_nodes:
+            children.append(text_node_to_html_node(node))
+    elif block_text.startswith("## "):
+        tag = "h2"
+        heading_text = block_text[3:]
+        text_nodes = text_to_textnodes(heading_text)
+        for node in text_nodes:
+            children.append(text_node_to_html_node(node))
+    elif block_text.startswith("### "):
+        tag = "h3"
+        heading_text = block_text[4:]
+        text_nodes = text_to_textnodes(heading_text)
+        for node in text_nodes:
+            children.append(text_node_to_html_node(node))
+    elif block_text.startswith("#### "):
+        tag = "h4"
+        heading_text = block_text[5:]
+        text_nodes = text_to_textnodes(heading_text)
+        for node in text_nodes:
+            children.append(text_node_to_html_node(node))
+    elif block_text.startswith("##### "):
+        tag = "h5"
+        heading_text = block_text[6:]
+        text_nodes = text_to_textnodes(heading_text)
+        for node in text_nodes:
+            children.append(text_node_to_html_node(node))
+    elif block_text.startswith("###### "):
+        tag = "h6"
+        heading_text = block_text[7:]
+        text_nodes = text_to_textnodes(heading_text)
+        for node in text_nodes:
+            children.append(text_node_to_html_node(node))
+    return ParentNode(tag, children)
+
+def quote_to_html_node(block_text):
+    lines = block_text.split("\n")
+    clean_lines = []
+    for line in lines:
+        if line.startswith("> "):
+            clean_lines.append(line[2:])
+        elif line.startswith(">"):
+            clean_lines.append(line[1:])
+        else:
+            clean_lines.append(line)
+    cleaned_text = "\n".join(clean_lines)
+    text_nodes = text_to_textnodes(cleaned_text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return ParentNode("blockquote", children)
+
+def code_to_html_node(block_text):
+    lines = block_text.split("\n")
+    code_lines = lines[1:-1]
+    code_content = "\n".join(code_lines) + "\n"
+    code_leaf = LeafNode("code", code_content)
+    return ParentNode("pre", [code_leaf])
--- src/test_blockline.py
+++ src/test_blockline.py
@@ -1,5 +1,8 @@
 import unittest
-from blockline import markdown_to_blocks, block_to_block_type, BlockType
+from blockline import (markdown_to_blocks, block_to_block_type, 
+                    BlockType, markdown_to_html_node,
+                    paragraph_to_html_node, quote_to_html_node,
+                    heading_to_html_node,)
 
 class Test_blockline(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -71,8 +74,64 @@ two. Second
         is_unordered = block_to_block_type(block)
         self.assertEqual(is_unordered, BlockType.UNORDERED_LIST)
 
+#these test markdown_to_html_node and helper functions
+    def test_markdown_to_html_paragraph(self):
+        md_p = """
+This is **bolded** paragraph
 
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+"""
+        expected = (
+    "<div><p>This is <b>bolded</b> paragraph</p>"
+    "<p>This is another paragraph with <i>italic</i> text and <code>code</code> here\n"
+    "This is the same paragraph on a new line</p></div>"
+)
+        converted = markdown_to_html_node(md_p).to_html()
+        self.assertEqual(expected, converted)
 
+    def test_markdown_to_html_heading(self):#this test showed a issue when headings were not seperated by "\n\n", be aware.
+        md_h = """# Heading One
+
+## Heading Two with _italic_
+
+### **BOLD** Heading Three"""
+        expected = (
+    "<div>"
+    "<h1>Heading One</h1>"
+    "<h2>Heading Two with <i>italic</i></h2>"
+    "<h3><b>BOLD</b> Heading Three</h3>"
+    "</div>"
+)
+        converted = markdown_to_html_node(md_h).to_html()
+        self.assertEqual(expected, converted)
+
+    def test_markdown_to_html_quote(self):
+        md_q = """> This is a quote with _italics_ and **bold** text
+> And a second quoted line
+
+A normal paragraph after."""
+        expected = (
+    "<div>"
+    "<blockquote>This is a quote with <i>italics</i> and <b>bold</b> text\nAnd a second quoted line</blockquote>"
+    "<p>A normal paragraph after.</p>"
+    "</div>"
+)
+        converted = markdown_to_html_node(md_q).to_html()
+        self.assertEqual(expected, converted)
+
+    def test_markdown_to_html_code(self):
+        md_c = """```
+def fib(n):
+    return n if n <= 1 else fib(n-1) + fib(n-2)
+```"""
+        expected = (
+    "<div><pre><code>def fib(n):\n"
+    "    return n if n <= 1 else fib(n-1) + fib(n-2)\n"
+    "</code></pre></div>"
+)
+        converted = markdown_to_html_node(md_c).to_html()
+        self.assertEqual(expected, converted)
 
 if __name__ == "__main__":
     unittest.main()
